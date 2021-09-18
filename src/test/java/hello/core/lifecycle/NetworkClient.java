1eb73971aebc0759221efe4b8984370e69b64849
JIYULLEE <jiyul147@gmail.com>
2021-09-18 11:54:26 +0900

    2021/09/18

---
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,45 @@
+package hello.core.lifecycle;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class NetworkClient {
+
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url =" + url);
+
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void connect() {
+        System.out.println("connect: " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call: " + url + " message = " + message);
+    }
+
+    //서비스 종료시 호출
+    public void disconnect() {
+        System.out.println("close: " + url);
+    }
+
+    @PostConstruct
+    public void init() throws Exception {
+        // 스프링이 의존관계 주입이 끝나면 이것을 콜해서 초기화 함.
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    @PreDestroy
+    public void close() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
+    }
+}
